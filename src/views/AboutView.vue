a47ddc832590b6de5ec354e322a046a8abc9c354
Lucien Cartier-Tilet <lucien@phundrak.com>
2023-01-22 19:36:12 +0100

    Update About page

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,11 +1,13 @@
 <template>
   <div class="about">
     <h2>About</h2>
+    <h3>Frontend</h3>
     <p>
       This website is a front-end for my custom document indexer. It runs with
       <a href="https://vuejs.org/">Vue.js</a> with
       <a href="https://www.typescriptlang.org/" target="_blank">Typescript</a>.
     </p>
+    <h3>Backend</h3>
     <p>
       On the other hand, the backend runs with
       <a href="https://www.rust-lang.org/" target="_blank">Rust</a>. Its web
@@ -15,6 +17,11 @@
       <a href="https://www.postgresql.org/" target="_blank">PostgreSQL</a> is
       powered by <a href="https://diesel.rs/" target="_blank">Diesel</a>.
     </p>
+    <p>
+      Documents not already available online are stored on an S3 data storage
+      hosted by <a href="https://www.scaleway.com">Scaleway</a> in France.
+    </p>
+    <h3>Source code</h3>
     <p>
       The source code for the frontend is available right now on
       <a
@@ -27,6 +34,7 @@
       The source code for the backend is unfortunately unavailable for now, but
       it will be free to access hopefully somewhat soon.
     </p>
-    <p>This page was last updated on November 25th, 2022.</p>
+    <h3>Last update</h3>
+    <p>This page was last updated on January 22nd, 2023.</p>
   </div>
 </template>
